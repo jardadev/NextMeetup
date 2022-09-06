2c75ee5565053edffbbb1990853a8c4b37852d21
Aaron Johnson <sugarwaterpurple.js@gmail.com>
2022-09-05 21:42:18 -0700

    add Event Filter Component

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,12 @@
 import { getAllEvents } from '../../dummy-data';
 import EventList from '../../components/Events/EventList/EventList';
+import EventFilter from '../../components/Events/EventFilter/EventFilter';
+
 const EventsPage = () => {
 	const events = getAllEvents();
 	return (
 		<div>
+			<EventFilter />
 			<EventList items={events} />
 		</div>
 	);

b33400880eb67bfd548af7d2ed4bc408f4f85d44
Aaron Johnson <sugarwaterpurple.js@gmail.com>
2022-09-05 21:41:56 -0700

    add single event display

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,5 +1,26 @@
+import { useRouter } from 'next/router';
+import { getEventById } from '../../dummy-data';
+import EventSummary from '../../components/Events/EventDetail/EventSummary';
+import EventContent from '../../components/Events/EventDetail/EventContent';
+import EventLogistics from '../../components/Events/EventDetail/EventLogistics';
 const EventDetailPage = () => {
-	return <div>One Single Event</div>;
+	const router = useRouter();
+	const eventDetail = router.query.eventId;
+
+	const event = getEventById(eventDetail);
+
+	return (
+		<>
+			<EventSummary title={event.title} />
+			<EventLogistics
+				date={event.date}
+				address={event.location}
+				image={event.image}
+				imageAlt={event.title}
+			/>
+			<EventContent>{event.description}</EventContent>
+		</>
+	);
 };
 
 export default EventDetailPage;

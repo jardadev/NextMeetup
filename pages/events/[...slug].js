2d54c1f30f0781988da83949518b544c231e6750
Aaron Johnson <sugarwaterpurple.js@gmail.com>
2022-09-06 17:22:09 -0700

    add filtered events page and logic

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,7 +1,35 @@
-import EventFilter from '../../components/Events/EventFilter/EventFilter'
+import EventFilter from '../../components/Events/EventFilter/EventFilter';
+import EventList from '../../components/Events/EventList/EventList';
+import { getFilteredEvents } from '../../dummy-data';
+import { useRouter } from 'next/router';
 
 const EventFilterPage = () => {
-	return <>Filtered Page</>;
+	const router = useRouter();
+	const filterData = router.query.slug;
+	console.log(filterData);
+	if (!filterData) {
+		return <p className='center'>Loading...</p>;
+	}
+
+	const filteredMonth = filterData[0];
+	const filteredYear = filterData[1];
+	const numMonth = +filteredMonth;
+	const numYear = +filteredYear;
+
+	const filteredEvents = getFilteredEvents({
+		year: numYear,
+		month: numMonth,
+	});
+	console.log(filteredEvents);
+	if (!filteredEvents || filteredEvents.length === 0) {
+		return <p className='center'>No events found for selected filters.</p>;
+	}
+
+	return (
+		<>
+			<EventList items={filteredEvents} />
+		</>
+	);
 };
 
 export default EventFilterPage;
